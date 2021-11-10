8c15aa4ac8b223725c7a5131736a5ab5814ad4a4
quickservicesltc <quickservicesltc@gmail.com>
2021-11-10 12:13:34 +0200

    partial

--- mai_pos_kitchen_screen_arabic/__manifest__.py
+++ mai_pos_kitchen_screen_arabic/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'POS Kitchen Receipt Arabic || POS Arabic Receipt with Kitchen order Receipt',
     'description': """
      Using This module you can print POS Kitchen Receipt Arabic with one is for a customer and one for a kitchen cook for order details.
-    """,    
+    """,
     'version': '3.2.1',
     'sequence': 1,
     'email': 'apps@maisolutionsllc.com',
@@ -12,14 +12,15 @@
     'author': 'MAISOLUTIONSLLC',
     'price': 15,
     'currency': 'EUR',
-    'license': 'OPL-1',    
+    'license': 'OPL-1',
     'depends': ['point_of_sale', 'pos_restaurant'],
-    # "live_test_url" : "",    
+    # "live_test_url" : "",
     'qweb': [
              'static/src/xml/templates.xml',
              ],
     'data': ['views/pos_receipt_template.xml',
              'views/product_view.xml',
+             'views/config_view.xml',
              ],
     'images': ['static/description/main_screenshot.png'],
     "live_test_url" : "https://youtu.be/cOFhcIrrWMM ",
@@ -28,4 +29,3 @@
     'auto_install': False,
     'application': True,
 }
-
